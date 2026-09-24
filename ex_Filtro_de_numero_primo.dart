283fcb686f59c7502d8976cb581586c4d237c22b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-23 22:34:46 -0300

    exercicio3

--- ex_Filtro_de_numero_primo.dart
+++ ex_Filtro_de_numero_primo.dart
@@ -1,4 +1,6 @@
-void mostrarResultado(List<int> numeros, List<int> numerosPrimos, List<int> numerosNaoPrimos) {
+
+
+void mostrarResultado( List<int> numerosPrimos, List<int> numerosNaoPrimos) {
   print('Números primos: $numerosPrimos');
   print('Números não primos: $numerosNaoPrimos');
 }
@@ -7,4 +9,20 @@ void main(){
   List<int> numeros = [2, 15, 17, 21, 29, 33, 41, 49];
   List<int> numerosPrimos = [];
   List<int> numerosNaoPrimos = [];
+
+  for (var numero in numeros) {
+    bool primo = true;
+    for (var divisor = 2; divisor < numero; divisor++) {
+      if (numero % divisor == 0) {
+        primo = false;
+
+      }
+    }
+    if (primo) {
+      numerosPrimos.add(numero);
+    } else {
+      numerosNaoPrimos.add(numero);
+    }
+  }
+  mostrarResultado(numerosPrimos, numerosNaoPrimos);
 }
